b235e003c6f3e55b2d0c196317b5012c0fd73f64
Crach1015 <crach@CrachTvListPro.local>
2015-12-16 20:24:30 -0400

    CrachTvListPro

---
+++ LIstas/raw fitnes.php
@@ -0,0 +1,107 @@
+#EXTM3U
+#EXTINF:-1,[COLOR yellow]RUTINA DE EJERCICIOS[/COLOR]
+
+
+
+
+
+#EXTINF:-1,[COLOR yellow]Ab Scult[/COLOR]
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!370&authkey=!ADYGtHY3D-
+_zQhk&ithint=video%2cavi
+
+#EXTINF:-1,[COLOR yellow]Ab Scult[/COLOR]
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!370&authkey=!ADYGtHY3D-_zQhk&ithint=video%2cavi
+
+#EXTINF:-1,[COLOR yellow]Fat Burning Cardio[/COLOR]
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!670&authkey=!AA3NkNOqJ7GO6p0&ithint=video%2cavi
+
+#EXTINF:-1,[COLOR yellow]Hips,Buns and Thighs[/COLOR]
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!673&authkey=!AILhKSFm7FEoQrw&ithint=video%2cavi
+
+#EXTINF:-1,[COLOR yellow]Last Minute Abs[/COLOR]
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!672&authkey=!AAPBmLHlWbbRYno&ithint=video%2cavi
+
+#EXTINF:-1,[COLOR yellow]Total Body Burn[/COLOR]
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!674&authkey=!AG8Rr3QGmPWldks&ithint=video%2cavi
+
+
+#EXTINF:-1,[COLOR orange]ZUMBATON[/COLOR]
+
+
+
+
+
+#EXTINF:-1,[COLOR yellow]Zumbaton[/COLOR] 
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!678&authkey=!AEnaC4547KFhPhI&ithint=video%2cavi
+#EXTINF:-1,[COLOR yellow]Zumbaton[/COLOR] 
+https://onedrive.live.com/download.aspx?resid=9E76003DB746BC22!678&authkey=!AEnaC4547KFhPhI&ithint=video%2cavi
+#EXTINF:-1,[COLOR yellow]Fitness Jillian[/COLOR]
+http://178.33.228.192:1935/vod/mp4:Jillian1.mp4/playlist.m3u8
+
+
+
+
+
+
+#EXTINF:-1,[COLOR orange]ESSENTRICS WORKOUT[/COLOR]
+
+
+
+#EXTINF:-1,[COLOR yellow]Essentrics Workout  1[/COLOR]  
+https://dl.dropbox.com/s/5s7qximee2p38pm/Essentrics%20Workout%20-1.mp4
+#EXTINF:-1,[COLOR yellow]Essentrics Workout  2[/COLOR] 
+https://dl.dropbox.com/s/igg1tqaolv93obb/Essentrics%20Workout%20-2.mp4
+
+
+
+#EXTINF:-1,[COLOR orange]21 DAY FIX EXTREME[/COLOR]
+
+
+#EXTINF:-1,[COLOR yellow]10 MINUTOS HARDCORE[/COLOR]
+https://dl.dropbox.com/s/ayeg5fjbuud78kb/10%20Minute%20hardcore.avi
+#EXTINF:-1,[COLOR yellow]ABC EXTREME[/COLOR]
+https://dl.dropbox.com/s/3tmbln81i2jtxjo/Abc%20extreme.avi
+#EXTINF:-1,[COLOR yellow]CARDIO EXTREME[/COLOR]
+https://dl.dropbox.com/s/ih8imp1a7koz4fo/Cardio%20extreme.avi
+#EXTINF:-1,[COLOR yellow]DIRTY 30 EXTREME[/COLOR]
+https://dl.dropbox.com/s/ulhegfo0uedtlc7/Dirty%2030%20extreme.avi
+#EXTINF:-1,[COLOR yellow]LOWER FIX EXTREME[/COLOR]
+https://dl.dropbox.com/s/v876da535iywn65/Lower%20fix%20Extreme.avi
+#EXTINF:-1,[COLOR yellow]PILATES ETREME[/COLOR]
+https://dl.dropbox.com/s/je0f83sirclxo41/Pilates%20extreme.avi
+#EXTINF:-1,[COLOR yellow]PLYO FIX EXTREME[/COLOR]
+https://dl.dropbox.com/s/pkngyqrysr2eb7l/Plyo%20fix%20extreme.avi
+#EXTINF:-1,[COLOR yellow]POWER STREGTH EXTREME[/COLOR]
+https://dl.dropbox.com/s/gbwmh4yqzdxeiv8/Power%20strength%20extreme.avi
+#EXTINF:-1,[COLOR yellow]THE CHALLENGE[/COLOR]
+https://dl.dropbox.com/s/ux7yesth8faklrr/The%20challenge.avi
+#EXTINF:-1,[COLOR yellow]UPPER EXTREME[/COLOR]
+https://dl.dropbox.com/s/n8to0p3a3zrz7ev/Upper%20extreme.avi
+#EXTINF:-1,[COLOR yellow]YOGA EXTREME[/COLOR]
+https://dl.dropbox.com/s/g2zyhvu9aviuoha/Yoga%20extreme.avi
+
+
+#EXTINF:-1,[COLOR red]JIU-JITSU[/COLOR]
+
+
+
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -The Back Position[/COLOR]
+https://dl.dropbox.com/s/3x2lnlxsnhxwtqa/andre_galvao_back_position.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -The Back Position[/COLOR]
+https://dl.dropbox.com/s/3x2lnlxsnhxwtqa/andre_galvao_back_position.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -Half guard[/COLOR]
+https://dl.dropbox.com/s/6054hcpatvoygnj/andre_galvao_half_guard.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -Open Guard[/COLOR]
+https://dl.dropbox.com/s/4pu3k7gdbu9yd53/andre_galvao_open_guard.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -Passing Guard[/COLOR]
+https://dl.dropbox.com/s/qdypjzf80345bxb/andre_galvao_passing_guard.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -stand up Game[/COLOR]
+https://dl.dropbox.com/s/cw7qznnp0y65g8z/andre_galvao_stand_up_game.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -Half Guard[/COLOR]
+https://dl.dropbox.com/s/iynei53n853ob9z/Half%20Guard.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -Streetfighting V1[/COLOR]
+https://dl.dropbox.com/s/qjmxys77fn2kdoj/Brazilian%20Jiu%20Jitsu%20Incorp.%20For%20Streetfighting%20Vol%201.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -Streetfighting V2[/COLOR]
+https://dl.dropbox.com/s/q1o1ftx5w46awwj/Brazilian%20Jiu%20Jitsu%20Incorp.%20For%20Streetfighting%20Vol%202.avi
+#EXTINF:-1,[COLOR yellow]Jiu jitsu -Streetfighting V3[/COLOR]
+https://DL.dropbox.com/s/jebw5ms99owm7py/Brazilian%20Jiu%20Jitsu%20Incorp.%20For%20Streetfighting%20Vol%203.avi
